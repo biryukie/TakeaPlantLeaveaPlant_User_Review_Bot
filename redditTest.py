bf1f95cc862f9a932b3cc4b23275d8851fbf3258
frankenweenie <anyamixon@gmail.com>
2019-07-09 01:16:29 -0700

    comments

--- redditTest.py
+++ redditTest.py
@@ -9,12 +9,17 @@ pwd = credentials.readline().strip()
 
 reddit = praw.Reddit(client_id = cid, client_secret = csc, password = pwd, user_agent = "testscript by /u/eggpl4nt", username = usn)
 
+# test to make sure PRAW is working
 print(reddit.user.me())
 
+# set the sub to TakeaPlantLeaveaPlant
 sub = reddit.subreddit("TakeaPlantLeaveaPlant")
+
+# test getting user and setting flair
 redditUser = reddit.redditor("AutonomousBotanist")
 #sub.flair.set(redditUser, text = "test ★★★★☆", css_class = "usergreen")
 
+# get the user directory
 page = sub.wiki["userdirectory"]
 
 file = open("userStuff.txt", "wb")
