f4de76e73734a51b86a69f8891c9704199065a3d
frankenweenie <anyamixon@gmail.com>
2019-07-09 21:51:27 -0700

    removed majority of excessive comments. added FANCY excessive comments.

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ def ADD_USER_RATING(username, rating, url):
 	for i in range(len(contents)):
 		if contents[i].count("#") != 2:
 			continue
-		if ("##" + username) == contents[i].strip():
-			print(username + " in position " + str(i)) 
+		if ("##" + username).lower() == (contents[i].strip()).lower():
+			#print(username + " in position " + str(i)) 
 			userFound = True
 			break
 		# stop once we go past potential usernames
 		if LESS_THAN(("##" + username), contents[i].strip()):
-			print(("##" + username) + " is less than " + contents[i].strip())
+			#print(("##" + username) + " is less than " + contents[i].strip())
 			break
 
 	reviews = list()
@@ -26,31 +26,24 @@ def ADD_USER_RATING(username, rating, url):
 
 		# get the first review column
 		firstReviewIndex = i + 4
-		firstReview = contents[firstReviewIndex].strip()
-		firstReview = firstReview.replace(" ", "")
-		#print(firstReview)
-		ratingNum = list(filter(None, firstReview.split("|")))[0]
-		#print("Rating is [" + ratingNum + "]")
 
 		# find the last review, get how many reviews total
 		numOfReviews = 0
 
 		while contents[firstReviewIndex + numOfReviews].find("|") != -1:
-			#print(contents[firstReviewIndex + numOfReviews])
-			reviews.append(((contents[firstReviewIndex + numOfReviews].strip()).replace(" ", "")).split("|")[1])
-			#print("{" + ((contents[firstReviewIndex + numOfReviews].strip()).replace(" ", "")).split("|")[1] + "}")
+			reviews.append(((contents[firstReviewIndex + numOfReviews].strip()).replace(" ", "")).split("|")[1])  # [1] because Python likes to have [0] be newline. very epic.
 			numOfReviews += 1
 		
 		insertionIndex = firstReviewIndex + len(reviews)
-		print("insert new row at index " + str(insertionIndex))
+		#print("insert new row at index " + str(insertionIndex))
 		s = "| " + rating + " | " + url + " |\r\n"
 		contents.insert(insertionIndex, s)
 
 	if not userFound:
-		print("USER [" + username + "] NOT FOUND")
-		print("we're at index " + str(i))
+		print("USER [" + username + "] NOT FOUND... CREATING SECTION...")
+		#print("we're at index " + str(i))
 		insertionIndex = i - 1;
-		print("insert new row at index " + str(insertionIndex))
+		#print("insert new row at index " + str(insertionIndex))
 		text = list()
 		text.append("\r\n##" + username + "\r\n")
 		text.append("###" + GET_FLAIR_TEXT(float(rating), 1) + "\r\n")
@@ -58,22 +51,13 @@ def ADD_USER_RATING(username, rating, url):
 		text.append("|--------|:-------|\r\n")
 		text.append("| " + rating + " | " + url + " |\r\n")
 		
-		print("-----")
-		for s in text:
-			print(s, end='')
-		print("-----")
-		
 		for s in reversed(text):
 			contents.insert(insertionIndex, s)
 
 		ratingIndex = insertionIndex + 1
 
-	print("rating index " + str(ratingIndex) + " : " + contents[ratingIndex])
-
 	# add current review
 	reviews.append(rating)
-
-	print("USER [" + username + "] HAS [" + str(len(reviews)) + "] REVIEWS")
 
 	# calculate average
 	avgRating = 0
@@ -82,10 +66,12 @@ def ADD_USER_RATING(username, rating, url):
 	
 	avgRating /= len(reviews)
 
+	# get flair text
 	flairText = GET_FLAIR_TEXT(avgRating, len(reviews))
 
 	print("USER [" + username + "] HAS RATING [" + flairText + "]")
 
+	# update rating in wikipage
 	contents[ratingIndex] = contents[ratingIndex].replace(contents[ratingIndex], "###" + flairText + "\n")
 
 	f = open("userStuff.txt", "wb")
@@ -93,7 +79,18 @@ def ADD_USER_RATING(username, rating, url):
 	f.write(contents.encode("utf-8"))
 	f.close()
 
+
 def GET_FLAIR_TEXT(rating, trades):
+	"""Generates the text to be used in a user's user flair.
+
+	Args:
+		rating: The average rating for the user (float).
+		trades: The number of trades the user has done (int).
+
+	Returns:
+		A string in the format of "***** (X, Y trades)"
+
+	"""
 	flairText = ""
 	roundedNum = round(rating + 0.0000001) # adding 0.0000001 because Python is stupid and does stupid rounding. Like all its stupid everything. SURPRISE!!
 
@@ -109,6 +106,19 @@ def GET_FLAIR_TEXT(rating, trades):
 	return flairText
 
 def LESS_THAN(a, b):
+	"""Used to sort username strings in alphanumeric order.
+
+	Python's own comparator would say that "Zebra" is less than "apple."
+	Length also needs to be taken into consideration, "Cat" is smaller than "Catty."
+
+	Args:
+		a: left string.
+		b: right string.
+
+	Returns:
+		True if a is smaller than b, False otherwise.
+
+	"""
 	length = a if len(a) < len(b) else b
 
 	_a = a.lower()
